3ea9dcf2da1fa1769856d4d76aa1c4c311bc6e4d
Carl Johnson <me@carlmjohnson.net>
2021-10-05 12:03:03 -0400

    Add ExampleCond

---
+++ cond_example_test.go
@@ -0,0 +1,25 @@
+package truthy_test
+
+import (
+	"fmt"
+
+	"github.com/carlmjohnson/truthy"
+)
+
+func ExampleCond_lazy() {
+	i := 1
+	// Cond cannot lazily evaluate its arguments,
+	// but you can use a closure to fake it.
+	s := truthy.Cond(i,
+		func() string {
+			// do some calculation
+			return "true"
+		},
+		func() string {
+			// do some calculation
+			return "false"
+		})()
+	fmt.Printf("%q\n", s)
+	// Output:
+	// "true"
+}
